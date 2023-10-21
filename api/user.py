9549fbfd95d82cd39ec1097cfaddb64e10aee1a3
rohan-sonthalia <sonthaliarohan@gmail.com>
2023-10-21 10:34:36 -0400

    fix

--- api/user.py
+++ api/user.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
+import openai
+import requests
 
 app = Flask(__name__)
 
-# In a real application, you'd use a database. For simplicity, we'll use a dictionary.
 users = {}
+form_data = {}
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -27,5 +29,39 @@ def login():
 
     return jsonify({'message': 'Logged in successfully'}), 200
 
+@app.route('/form1', methods=['POST'])
+def form1():
+    form_data['form1'] = request.form.to_dict()
+    return jsonify({'message': 'Form 1 data stored successfully'}), 200
+
+@app.route('/form2', methods=['POST'])
+def form2():
+    form_data['form2'] = request.form.to_dict()
+    return jsonify({'message': 'Form 2 data stored successfully'}), 200
+
+@app.route('/form3', methods=['POST'])
+def form3():
+    form_data['form3'] = request.form.to_dict()
+    return jsonify({'message': 'Form 3 data stored successfully'}), 200
+
+@app.route('/form4', methods=['POST'])
+def form4():
+    form_data['form4'] = request.form.to_dict()
+    return jsonify({'message': 'Form 4 data stored successfully'}), 200
+
+@app.route('/openai', methods=['POST'])
+def gpt_call():
+    openai.api_key = "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"
+
+    # Example of calling the GPT-3 API
+    response = openai.Completion.create(
+        engine="gpt-3.5-turbo-instruct",
+        prompt=requests.json.get('prompt'),
+        max_tokens = 3500
+    )
+
+    output = response.choices[0].text
+    return jsonify({'output': output}), 200
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8000,debug=True)
